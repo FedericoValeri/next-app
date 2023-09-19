5544f88489d39f708e807ecb866747da76c469a6
Federico <federico.valeri@unicam.it>
2023-09-19 14:42:17 +0200

    Using Tailwind instead of CSS modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import ProductCard from "./components/ProductCard/ProductCard";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
