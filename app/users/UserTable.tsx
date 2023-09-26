e7eac84992d0fdffc1d4ed20cc15c7f6cefc20ae
Federico <federico.valeri@unicam.it>
2023-09-26 08:42:30 +0200

    Routing overview

---
+++ app/users/UserTable.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UserTable = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 }, // Get fresh data (no cached) from the backend abery 10 seconds
+  });
+  const users: User[] = await res.json();
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserTable;
