1194feeea9f7167e50efa5dd197c5401e1c4c5fc
Federico <federico.valeri@unicam.it>
2023-09-27 21:50:55 +0200

    API: creating an object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -7,3 +7,13 @@ export function GET(request: NextRequest) {
     { id: 2, name: "Giovanni" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  // Validation
+  // If invalid, return 400
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required." }, { status: 400 });
+  // Else, return created data
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
